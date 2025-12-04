26e9411c38c76da4955226da0d763197b55b7576
Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>
2025-12-04 09:52:59 +0100

    Upgraded Django & DRF
    Signed-off-by: Sascha Pfeiffer <sascha.pfeiffer@esaqa.com>

--- psono/cron/views/cleanup_chunks.py
+++ psono/cron/views/cleanup_chunks.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.core.files.storage import get_storage_class
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.generics import GenericAPIView
